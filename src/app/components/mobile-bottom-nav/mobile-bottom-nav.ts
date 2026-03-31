3e29cca46bc537f3a238a403b3ebcca8a0f3c25e
Aryan <119606904+aryster07@users.noreply.github.com>
2026-03-31 15:24:58 +0530

    Add new UI components (badge, card, checkbox, divider, etc)

---
+++ src/app/components/mobile-bottom-nav/mobile-bottom-nav.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-bottom-nav',
+  imports: [],
+  templateUrl: './mobile-bottom-nav.html',
+  styleUrl: './mobile-bottom-nav.css',
+})
+export class MobileBottomNav {}
---
+++ src/app/components/mobile-card/mobile-card.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile-card',
+  imports: [],
+  templateUrl: './mobile-card.html',
+  styleUrl: './mobile-card.css',
+})
+export class MobileCard {}
